d0acaaad9c709736977a2cc0aaed9216e5a175d0
Mark Lewis <mlewis@trinity.edu>
2023-10-29 11:20:53 -0500

    Updates to test code.

--- app/src/main/java/csci2320/App.java
+++ app/src/main/java/csci2320/App.java
@@ -8,26 +8,27 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             long seed = sc.nextLong();
-            ThreadLocalRandom rand = ThreadLocalRandom.current();
-            rand.setSeed(seed);
+            Random rand = new Random(seed);
+            int size = sc.nextInt();
+            String type = sc.next();
+            switch(type) {
+                case "basic":
+                    basicTest(rand, size);
+                    break;
+                case "mst":
+                    mstTest(rand, size, sc.nextDouble());
+                    break;
+            }
         }
     }
 
-    static String randomString(ThreadLocalRandom rand, int len) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < len; ++i) {
-            sb.append((char)('a'+rand.nextInt(26)));
-        }
-        return sb.toString();
-    }
-
-    public static void basicTest(ThreadLocalRandom rand, int size) {
+    public static void basicTest(Random rand, int size) {
         List<DisjointSet<Integer>> sets = new ArrayList<>();
         for (int i = 0; i < size; ++i) {
             sets.add(DisjointSet.makeSet(i));
@@ -39,7 +40,7 @@ public class App {
             if (sets.get(s1).findSet() != sets.get(s2).findSet()) {
                 sets.get(s1).union(sets.get(s2));
                 unionCount++;
-                if (!sets.get(s1).findSet().equals(sets.get(s2))) {
+                if (!sets.get(s1).findSet().equals(sets.get(s2).findSet())) {
                     System.out.println("Sets don't match after union.");
                 }
             }
@@ -51,7 +52,7 @@ public class App {
         System.out.println(size + " " + unionCount + " " + endSets.size());
     }
 
-    static List<MinimumSpanningTree.Edge> randomWeightedGraph(ThreadLocalRandom rand, int numVerts, double density) {
+    static List<MinimumSpanningTree.Edge> randomWeightedGraph(Random rand, int numVerts, double density) {
         var ret = new ArrayList<MinimumSpanningTree.Edge>();
         for (int i = 0; i < numVerts; ++i) {
             for (int j = i+1; j < numVerts; ++j) {
@@ -61,7 +62,7 @@ public class App {
         return ret;
     }
 
-    public static void mstTest(ThreadLocalRandom rand, int size, double density) {
+    public static void mstTest(Random rand, int size, double density) {
         var edges = randomWeightedGraph(rand, size, density);
         var mst = MinimumSpanningTree.kruskals(size, edges);
         var cnt = 0;
@@ -70,6 +71,6 @@ public class App {
             cnt += 1;
             weightSum += edge.weight();
         }
-        System.out.println(cnt + " " + weightSum);
+        System.out.printf("%d %1.4f", cnt, weightSum);
     }
 }
